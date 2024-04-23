2902660f50292db7d6ebc3b4bde205dc36f70d67
welsey <follow@live.in>
2024-04-23 13:39:40 +0800

    优化代码

--- src/NodeService.ServiceHost/Program.cs
+++ src/NodeService.ServiceHost/Program.cs
@@ -14,7 +14,6 @@ using NodeService.ServiceHost.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Management.Automation;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Windows.Forms.Design.AxImporter;
