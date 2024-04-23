a44fbbac98b7cb18c567185758f4cc257cecf42c
welsey <follow@live.in>
2024-04-23 12:01:56 +0800

    升级版本

--- src/NodeService.ServiceHost/Constants.cs
+++ src/NodeService.ServiceHost/Constants.cs
@@ -8,7 +8,7 @@ namespace NodeService.ServiceHost
 {
     public class Constants
     {
-        public const string Version = "08E18336-019C-4DCE-93EA-3417229CA7A7";
+        public const string Version = "3CEA2E75-3350-443F-8E94-B5BA2089B318";
 
         public const string ServiceTypeWindowsService = "WindowsService";
 
