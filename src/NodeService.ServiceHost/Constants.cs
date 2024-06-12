09b2b9eb368e0aa5d5bceda610e620910d0a9201
dart <follow@live.in>
2024-06-12 17:45:01 +0800

    升级版本号

--- src/NodeService.ServiceHost/Constants.cs
+++ src/NodeService.ServiceHost/Constants.cs
@@ -2,7 +2,7 @@
 {
     public class Constants
     {
-        public const string Version = "7F42111B-7774-4129-B5CD-96A51E84952F";
+        public const string Version = "43D74ED9-2722-404D-842E-7D19E090A7BB";
 
         public const string ServiceTypeWindowsService = "WindowsService";
 
