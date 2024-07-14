2349347121fbec2d8c36f9810ecd76a44e661c18
dart <follow@live.in>
2024-07-14 23:57:26 +0800

    运行PYTHON代码。

--- src/NodeService.ServiceHost/Tasks/ExecutePythonCodeTask.cs
+++ src/NodeService.ServiceHost/Tasks/ExecutePythonCodeTask.cs
@@ -24,14 +24,49 @@ namespace NodeService.ServiceHost.Tasks
                 Logger.LogError("no code");
                 return;
             }
-            PythonEngine.Initialize();
-            // call Python's sys.version to prove we are executing the right version
+
+
             dynamic sys = Py.Import("sys");
-            Logger.LogInformation($"### Python version:\n\t{sys.version}");
-            using (Py.GIL())
-            {
-                PythonEngine.Exec(options.Code);
-            }
+            Logger.LogInformation($"### Python version:{Environment.NewLine}{sys.version}");
+
+            using var scope = Py.CreateScope();
+            scope.Exec(
+                """
+                import sys
+
+                class NetConsole(object):
+                    def __init__(self, writeCallback):
+                        self.terminal = sys.stdout
+                        self.writeCallback = writeCallback
+
+                    def write(self, message):
+                        self.terminal.write(message)
+                        self.writeCallback(message)
+
+                    def flush(self):
+                        # this flush method is needed for python 3 compatibility.
+                        # this handles the flush command by doing nothing.
+                        # you might want to specify some extra behavior here.
+                        pass
+
+                def setConsoleOut(writeCallback):
+                    sys.stdout = NetConsole(writeCallback)
+                """
+                );
+
+            dynamic setConsoleOutFn = scope.Get("setConsoleOut");
+
+            setConsoleOutFn(new Action<string>(Print));
+
+            scope.Exec(options.Code);
+        }
+
+        void Print(string message)
+        {
+            if (message != Environment.NewLine)
+                Logger.LogInformation("[Py]: ");
+
+            Logger.LogInformation(message);
         }
     }
 }
